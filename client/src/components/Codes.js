74059ae6bb67444de5f2749c03ad2d795121fccd
Deepak kushwah <90465100+Deepakkushwahg@users.noreply.github.com>
2023-07-13 23:49:22 +0530

    Create Codes.js

---
+++ client/src/components/Codes.js
@@ -0,0 +1,49 @@
+import React,{useEffect, useState} from 'react'
+import '../css-style/Codes.css';
+import copyImage from '../images/copy.png';
+import leetcode from '../images/leetcode.png'
+const BASE_URL = 'http://localhost:8080';
+export default function Codes() {
+
+  const handleCopy = (copyText)=>{
+    try{
+      navigator.clipboard.writeText(copyText);
+      console.log('text copied');
+    }
+    catch(e){
+      console.log('error to copy text');
+    }
+  }
+  const [data, setData] = useState([]);
+  const getUsers = async ()=>{
+    const response = await fetch(`${BASE_URL}/getQuestions`,{
+      method:'GET',
+    })
+   const data = await response.json();
+   setData(data);
+  }
+
+  useEffect(()=>{
+    getUsers();
+  },[])
+  
+  return (
+    <div id="codes">
+                <div id="leetcode-logo">
+                    <img src={leetcode} alt="leetcode.png" width="300px"/>
+                    <h1 id='text'>Leetcode Solutions</h1>
+                </div><br/><br/>
+                <div id='questions'>
+                    {data.map(doc => (
+                      <>
+                      <h1 id={doc.q_no} key={doc._id}>{doc.q_no}. {doc.question}</h1>
+                      <div class="box">
+                      <abbr title="copy"><img id='copy-to-clipboard' src={copyImage} alt="copy.png" onClick={()=>handleCopy(doc.code)}/></abbr>
+                      <div id='code'><code><pre>{doc.code}</pre></code></div>
+                      </div>
+                      </>
+                    ))}
+                </div>
+            </div>
+  )
+}
