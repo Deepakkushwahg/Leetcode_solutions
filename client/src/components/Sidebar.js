907d830fb26612618d334e89873470fb6ee73829
Deepak kushwah <90465100+Deepakkushwahg@users.noreply.github.com>
2023-07-13 23:49:49 +0530

    Add files via upload

---
+++ client/src/components/Sidebar.js
@@ -0,0 +1,29 @@
+import React,{useEffect, useState} from 'react'
+import '../css-style/Sidebar.css';
+const BASE_URL = 'http://localhost:8080';
+export default function Sidebar() {
+  const [data, setData] = useState([]);
+  const getUsers = async ()=>{
+    const response = await fetch(`${BASE_URL}/getQuestions`,{
+      method:'GET',
+    })
+   const data = await response.json();
+   setData(data);
+  }
+
+  useEffect(()=>{
+    getUsers();
+  },[])
+  return (
+    <div id="sidebar">
+        <ul>
+            <li><h2>Problems -</h2></li>
+            <ul id="questions-no">
+                {data.map(doc => (
+                    <li><a href={"#"+doc.q_no} key={doc._id}>{doc.q_no}. {doc.question}</a></li>        
+                ))}
+            </ul>
+        </ul>
+    </div>
+  )
+}
