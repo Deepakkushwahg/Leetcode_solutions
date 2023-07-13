d620fd9314d6cd7ceb35afeb3d6679b51916c34d
Deepak kushwah <90465100+Deepakkushwahg@users.noreply.github.com>
2023-07-13 23:47:40 +0530

    Add files via upload

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+
+import Header from './components/Header';
+import BodyContent from './components/BodyContent';
+function App() {
+  return (
+    <div id='container'>
+      <Header/>
+      <BodyContent/>
+    </div>
+  );
+}
+
+export default App;
