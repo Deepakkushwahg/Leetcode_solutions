68ffa32601964a87ef25a793b88598955eb1baed
Deepak kushwah <90465100+Deepakkushwahg@users.noreply.github.com>
2024-06-06 15:50:10 +0530

    Update Header.js

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -9,13 +9,13 @@ export default function Header() {
     <nav>
         <div id="header">
             <div id="leetcode">
-                <img src={leetcode} alt="leetcode.png" width="55px"/>
+                <img src={leetcode} alt="leetcode.png" width="5.5%"/>
                 <h1>Leetcode Solutions Using Java</h1>
             </div>
-            <a id="profile" href="https://github.com/Deepakkushwahg/leetcode">
-                <img src={git} alt="git.png" width="30px"/> <span>Deepakkushwahg/Leetcode</span>
-            </a>
         </div>
+        <a id="profile" href="https://github.com/Deepakkushwahg/leetcode">
+                <img src={git} alt="git.png" width="12%"/> <span>Deepakkushwahg/Leetcode</span>
+        </a>
     </nav>
   )
 }
